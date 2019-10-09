e0ac08f51864766e219b7750bcb9cd5f1d640616
xuezhiyong <xuezhiyong@diamondcat.com>
2019-10-09 23:38:09 +0800

    购物车

--- shop1/pages/cart/cart.js
+++ shop1/pages/cart/cart.js
@@ -5,62 +5,65 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isSelectAll: false,
+    totalPrice: 0,
+    totalCount: 0,
+    list: [{
+      image: "/assets/images/girl5.jpg",
+      title: "2019最新款",
+      desc: "2019最新款2019最新款2019最新款2019最新款",
+      price: 30,
+      count: 2
+    }, {
+      image: "/assets/images/girl5.jpg",
+      title: "2019最新款",
+      desc: "2019最新款2019最新款2019最新款2019最新款",
+      price: 30,
+      count: 2
+    }, {
+      image: "/assets/images/girl5.jpg",
+      title: "2019最新款",
+      desc: "2019最新款2019最新款2019最新款2019最新款",
+      price: 30,
+      count: 2
+    }, {
+      image: "/assets/images/girl5.jpg",
+      title: "2019最新款",
+      desc: "2019最新款2019最新款2019最新款2019最新款",
+      price: 30,
+      count: 2
+    }, {
+      image: "/assets/images/girl5.jpg",
+      title: "2019最新款",
+      desc: "2019最新款2019最新款2019最新款2019最新款",
+      price: 30,
+      count: 2
+    }, {
+      image: "/assets/images/girl5.jpg",
+      title: "2019最新款",
+      desc: "2019最新款2019最新款2019最新款2019最新款",
+      price: 30,
+      count: 2
+    }, {
+      image: "/assets/images/girl5.jpg",
+      title: "2019最新款",
+      desc: "2019最新款2019最新款2019最新款2019最新款",
+      price: 30,
+      count: 2
+    }, {
+      image: "/assets/images/girl5.jpg",
+      title: "2019最新款",
+      desc: "2019最新款2019最新款2019最新款2019最新款",
+      price: 30,
+      count: 2
+    }, ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
---
+++ shop1/pages/cart/cc/x-bottom-bar/x-bottom-bar.js
@@ -0,0 +1,32 @@
+// pages/cart/cc/x-bottom-bar/x-bottom-bar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    selected: {
+      type: Boolean,
+      value: true
+    },
+    total_price: {
+      type: Number
+    },
+    counter: {
+      type: Number
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
